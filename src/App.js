57ccee1feba262a826a689a62e551d07fe876841
shanemoyo <smoyo@pdx.edu>
2020-06-22 08:05:13 -0700

    add about page.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import React from 'react';
 import './App.css';
 import Gists from './components/gists/Gists'; 
+import { 
+  BrowserRouter as Router, 
+  Route, 
+  Switch, 
+  Link,
+ } from 'react-router-dom'
 
 function App() {
+  const About = <section> <h1>About me</h1> <p> HI I am shane!</p></section>
   return (
     <div className="App">
-      <Gists/>
+      <Router> 
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link> 
+            </li>
+            <li>
+              <Link to="/about">About</Link> 
+            </li>
+          </ul>
+        </nav>
+        <Switch> 
+          
+          <Route path="/about">
+            {About}
+          </Route>
+          <Route path="/" component={Gists}/>
+        </Switch> 
+      </Router>
     </div>
   );
 }
